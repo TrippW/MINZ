b5143109699b393c83b4755880e32038a9d0d611
Tripp Weiner <trwlll@mindspring.com>
2017-01-31 20:51:55 -0600

    Organize Main file

--- MINZ/src/minz/MINZ.java
+++ MINZ/src/minz/MINZ.java
@@ -4,32 +4,35 @@ import static minz.Algorithm.alphabeta;
 
 public class MINZ {
 
-    
     public static void main(String[] args) {
-        MINZ game = new MINZ(3, 4);
+        MINZ game = new MINZ(3, 5);
         game.print();
-        while(!game.board.check_for_win() && !game.cur_node.get_children().isEmpty()){
-            game.cur_node = alphabeta(game.cur_node, 5);
-            game.board.play(game.cur_node.move);
-            game.print();
+        int i = 0;
+        while (!game.board.check_for_win() && !game.cur_node.get_children().isEmpty()) {
+            game.play(i++ % 2);
+
         }
+    }
+
+    public void play(int i) {
+        cur_node = alphabeta(cur_node, 2 + (3 * i));
+        board.play(cur_node.move);
+        cur_node.fill_children(1);
+        print();
     }
 
     private Board board;
     private Node cur_node;
-    
+
     public static final char PLAYER1 = 'x',
             PLAYER2 = 'o';
 
     public MINZ(int m, int z) {
         board = new Board(z, m);
-        cur_node = new Node(board, null);
+        cur_node = new Node(board, null,5);//new Move(z/2,z/2,PLAYER1),3);
     }
 
     public void print() {
         board.print();
     }
-    
-    
-
 }
