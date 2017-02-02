6c838b2958f5c2875f5a6e5689962a30331ba485
Tripp Weiner <trwlll@mindspring.com>
2017-02-01 21:20:58 -0600

    Improved Algorithm
    Algorithm returns integer value instead of a node now.

--- MINZ/src/minz/Algorithm.java
+++ MINZ/src/minz/Algorithm.java
@@ -1,52 +1,43 @@
 package minz;
 
 public class Algorithm {
-
+    private static Node best_node;
     public static Node alphabeta(Node node, int depth) {
-        return alphabeta(node, depth, Integer.MIN_VALUE, Integer.MAX_VALUE, true);
+        alphabeta(node, depth, Integer.MIN_VALUE, Integer.MAX_VALUE, true);
+        return best_node;
     }
 
-    private static Node alphabeta(Node node, int depth, int a, int b, boolean is_maximizing) {
+    private static int alphabeta(Node node, int depth, int a, int b, boolean is_maximizing) {
         if (depth == 0 || node.get_children().isEmpty()) {
-            return node; //value?
+            return node.get_value(); //value?
         }
         if (is_maximizing) {
-            int v = Integer.MIN_VALUE;
-            Node max_node = node;
+            int n = 0;
             for (Node child : node.get_children()) {
-                Node n;
-                if(v < (n = alphabeta(child, depth - 1, a, b, false)).get_value()){
-                    v = n.get_value();
-                    max_node = child;
-                }
-                if(a < v){
-                    a = v;
-
+                n = alphabeta(child, depth - 1, a, b, false);
+                if(a < n){
+                    a = n;
+                    best_node = child;
                 }
                 //a = Math.max(a, v);
                 if (b <= a) {
                     break; //cut off
                 }
             }
-            return max_node;
+            return n;
         } else {
-            int v = Integer.MAX_VALUE;
-            Node min_node = node;
+            int n = 0;
             for (Node child : node.get_children()) {
-                Node n;
-                if(v > (n = alphabeta(child, depth - 1, a, b, true)).get_value()){
-                    v = n.get_value();
-                    min_node = child;
+                n = alphabeta(child, depth - 1, a, b, false);
+                if(b > n){
+                    b = n;
+                    //best_node = child;
                 }
-                if(a > v){
-                    a = v;
-                }
-//                a = Math.min(a, v);
                 if (b <= a) {
                     break; //cut off
                 }
             }
-            return min_node;
+            return n;
         }
     }
 }
